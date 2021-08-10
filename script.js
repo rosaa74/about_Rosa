ac5ef1ed7b32f5277fd1a3dc7b3b1106322185ed
Rosa Saldivar <88331961+rosaa74@users.noreply.github.com>
2021-08-10 20:11:52 +0000

    font and background

---
+++ script.js
@@ -0,0 +1,9 @@
+function getFormvalue(){
+  document.getElementById("display").innerHTML = "Wow! We have similar interests!";
+}
+function getLike(){
+  document.getElementById("like").innerHTML = "Thank you for the Like!";
+}
+function loadImage() {
+ alert("Image is loaded");
+}
